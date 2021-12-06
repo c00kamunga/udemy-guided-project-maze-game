f354c3cf53db2f8924bcd0037a0dd7e4fa4a70f7
Barinder <tsothalanti2@yahoo.com>
2021-12-06 13:09:10 -0800

    finished orientation for vertical walls

--- index.js
+++ index.js
@@ -70,19 +70,38 @@ const stepThroughCell = (row, column) => {
 
   //Assemble randomly-ordered list of neighbors
   const neighbors = shuffle([
-    [row - 1, column],
-    [row, column + 1],
-    [row + 1, column],
-    [row, column - 1],
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left'],
   ]);
 
   //for each neighbor...
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
 
-  //See if that neighbor is out of bounds
+    //See if that neighbor is out of bounds
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
 
-  //If we have visited that neighbor, continue to next neighbor
+    //If we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
 
-  //Remove a wall from either horizontals or verticals
+    //Remove a wall from either horizontals or verticals
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    }
+  }
 
   //Visit that next cell
 };
