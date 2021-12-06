77df43046bc359c4343ec4dbf82558e04c86de84
Barinder <tsothalanti2@yahoo.com>
2021-12-06 12:46:44 -0800

    created randomizer algorithm for neighboring cells surrounding starting cell

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-cells = 5;
+cells = 3;
 const width = 600;
 const height = 600;
 
@@ -28,6 +28,22 @@ const walls = [
 World.add(world, walls);
 
 //Maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -40,4 +56,35 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
-console.log(horizontals);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  //if i have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+
+  //Mark this cell as being visited
+  grid[row][column] = true;
+
+  //Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1],
+  ]);
+
+  //for each neighbor...
+
+  //See if that neighbor is out of bounds
+
+  //If we have visited that neighbor, continue to next neighbor
+
+  //Remove a wall from either horizontals or verticals
+
+  //Visit that next cell
+};
+
+stepThroughCell(1, 1);
