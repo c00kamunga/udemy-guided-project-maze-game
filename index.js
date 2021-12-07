0fe90f73dd57adbeec90f890bb63738210ea18be
Barinder <tsothalanti2@yahoo.com>
2021-12-06 20:59:43 -0800

    ball is now able to move in an up down left right direction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
 cells = 3;
 const width = 600;
@@ -168,19 +168,21 @@ const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
 World.add(world, ball);
 
 document.addEventListener('keydown', (event) => {
+  const { x, y } = ball.velocity;
+
   if (event.keyCode === 87) {
-    console.log('move ball up');
+    Body.setVelocity(ball, { x, y: y - 5 });
   }
 
   if (event.keyCode === 68) {
-    console.log('move ball right');
+    Body.setVelocity(ball, { x: x + 5, y });
   }
 
   if (event.keyCode === 83) {
-    console.log('move ball down');
+    Body.setVelocity(ball, { x, y: y + 5 });
   }
 
   if (event.keyCode === 65) {
-    console.log('move ball left');
+    Body.setVelocity(ball, { x: x - 5, y });
   }
 });
