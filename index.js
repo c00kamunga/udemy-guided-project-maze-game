6f41dca822429bfa20da7436fffd6b4d94bf7c28
Barinder <tsothalanti2@yahoo.com>
2021-12-07 16:54:12 -0800

    added collision event for when ball contacts goal shape

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 cells = 3;
 const width = 600;
@@ -7,6 +7,7 @@ const height = 600;
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -158,13 +159,16 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: 'goal',
     isStatic: true,
   }
 );
 World.add(world, goal);
 
 //BALL
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: 'ball',
+});
 World.add(world, ball);
 
 document.addEventListener('keydown', (event) => {
@@ -185,4 +189,19 @@ document.addEventListener('keydown', (event) => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+//Win Condition
+
+Events.on(engine, 'collisionStart', (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ['ball', 'goal'];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log('User won!');
+    }
+  });
 });
